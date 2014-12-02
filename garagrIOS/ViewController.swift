b3739db9747528f09ae420472198490dd87478a4
mgingras <martin@mgingras.ca>
2014-12-01 20:14:59 -0500

    Initial Commit

---
+++ garagrIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  garagrIOS
+//
+//  Created by Martin Gingras on 2014-12-01.
+//  Copyright (c) 2014 mgingras. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
